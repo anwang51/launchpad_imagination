b222256552ffe4db1fa14ffcdc16d76be54d432e
An Wang <anwang@berkeley.edu>
2018-02-20 22:23:37 -0800

    improved runner file

--- day3/Runner.py
+++ day3/Runner.py
@@ -1,6 +1,6 @@
 from ShipsEnv import ShipsEnv
 import DumbAI as dum
-import SmartAI as sm
+from SmartAI import DQNAgent
 import numpy as np
 
 env = ShipsEnv(False)
@@ -9,12 +9,14 @@ reward = 0
 games_count = 0
 wins = 0
 
+agent = DQNAgent(70, 4)
+
 num_games = 100
 i = 0
 while i < num_games:
 	print("iteration #", i)
 	while reward == 0:
-		player_ai = sm.act(vec)
+		player_ai = agent.act(vec)
 		opponent_ai = dum.act(vec)
 		reward, vec = env.step(player_ai, opponent_ai)
 	games_count += 1
@@ -23,6 +25,7 @@ while i < num_games:
 	i += 1
 	env.reset()
 	reward = 0
+	vec = env.game_vec
 	print("wins: ", wins)
 win_rate = float(wins)/games_count
 print(win_rate)

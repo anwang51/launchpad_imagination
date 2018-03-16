336deb15e2018ac9eade0bdcbbb3a66f1a8540e1
James Chen <Family@airbears2-10-142-29-24.airbears2.1918.berkeley.edu>
2018-03-15 22:04:05 -0700

    added self-contained train for dqn_agent

---
+++ day4/dqn_agent.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from collections import deque
+import numpy as np
+import random
+import gym
+
+record = open("performance", "w")
+
+# Deep Q-learning Agent
+class DQNAgent:
+
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = self._build_model()
+        self.episodes = 2000
+        self.training_result = []
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+              target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    # Should be def train(self, agent_action)
+    def train(self):
+        env = gym.make('CartPole-v1')
+        print(env.observation_space, env.action_space)
+        # Iterate the game
+        for e in range(self.episodes):
+            # reset state in the beginning of each game
+            state = env.reset()
+            state = np.reshape(state, [1, 4])
+            # time_t represents each frame of the game
+            # Our goal is to keep the pole upright as long as possible until score of 500
+            # the more time_t the more score
+            for time_t in range(500):
+                # turn this on if you want to render
+                env.render()
+                # Decide action
+                action = self.act(state)
+                # Advance the game to the next frame based on the action.
+                # Reward is 1 for every frame the pole survived
+                next_state, reward, done, _ = env.step(action)
+                reward = reward if not done else -10
+                next_state = np.reshape(next_state, [1, 4])
+                # Remember the previous state, action, reward, and done
+                self.remember(state, action, reward, next_state, done)
+                # make next_state the new current state for the next frame.
+                state = next_state
+                # done becomes True when the game ends
+                # ex) The agent drops the pole
+                if done:
+                    # print the score and break out of the loop
+                    print("episode: {}/{}, score: {}"
+                          .format(e, self.episodes, time_t))
+                    break
+            # train the agent with the experience of the episode
+            self.training_result.append(time_t)
+            num_mem = len(self.memory)
+            if num_mem > 32:
+                num_mem = 32
+            agent.replay(num_mem)
+        for e in self.training_result:
+            record.write(str(e) + " ")
+
+agent = DQNAgent(4,2)
+agent.train()
---
+++ day4/dqn_train.py
@@ -0,0 +1,51 @@
+import gym
+from dqn_agent import DQNAgent
+import settings
+import numpy as np
+
+episodes = settings.episodes
+
+record = open("performance", "w")
+training_result = []
+
+if __name__ == "__main__":
+    # initialize gym environment and the agent
+    env = gym.make('CartPole-v0')
+    print(env.observation_space, env.action_space)
+    agent = DQNAgent(4,2)
+    # Iterate the game
+    for e in range(episodes):
+        # reset state in the beginning of each game
+        state = env.reset()
+        state = np.reshape(state, [1, 4])
+        # time_t represents each frame of the game
+        # Our goal is to keep the pole upright as long as possible until score of 500
+        # the more time_t the more score
+        for time_t in range(500):
+            # turn this on if you want to render
+            env.render()
+            # Decide action
+            action = agent.act(state)
+            # Advance the game to the next frame based on the action.
+            # Reward is 1 for every frame the pole survived
+            next_state, reward, done, _ = env.step(action)
+            next_state = np.reshape(next_state, [1, 4])
+            # Remember the previous state, action, reward, and done
+            agent.remember(state, action, reward, next_state, done)
+            # make next_state the new current state for the next frame.
+            state = next_state
+            # done becomes True when the game ends
+            # ex) The agent drops the pole
+            if done:
+                # print the score and break out of the loop
+                print("episode: {}/{}, score: {}"
+                      .format(e, episodes, time_t))
+                break
+        # train the agent with the experience of the episode
+        training_result.append(time_t)
+        num_mem = len(agent.memory)
+        if num_mem > 32:
+            num_mem = 32
+        agent.replay(num_mem)
+    for e in training_result:
+        record.write(str(e) + " ")

261672ebc7903f565fe2da64a19fbf5e939fa124
Andrew <amdickson128@gmail.com>
2018-04-10 20:49:39 -0700

    Nearly working ICNetTraining

--- imagination_architecture/model_based/ICNet.py
+++ imagination_architecture/model_based/ICNet.py
@@ -1,17 +1,34 @@
 import tensorflow as tf
-from tensorflow.contrib import rnn
 import numpy as np
+gamma = 0.9
 class ICNet:
 	def __init__(sess, input_size, action_num):
+		self.sess = sess
 		self.x = tf.placeholder("float", [None, input_size])
-		W1 = tf.Variable("float", [input_size, 20])
-		b1 = tf.Variable("float", [20])
+		W1 = tf.Variable("float", tf.random_uniform([input_size, 20], -0.5, 0.5))
+		b1 = tf.Variable("float", tf.random_uniform([20], -0.5, 0.5))
 		l1 = tf.nn.relu(tf.matmul(x, W1)+b1)
-		W2 = tf.Variable("float", [20, action_num])
-		b2 = tf.Variable("float", [action_num])
+		W2 = tf.Variable("float", tf.random_uniform([20, action_num], -0.5, 0.5))
+		b2 = tf.Variable("float", tf.random_uniform([action_num], -0.5, 0.5))
 		self.y_hat = tf.nn.relu(tf.matmul(l1, W2)+b2)
-		self.labels = tf.placeholder("float", [None])
+		self.y = tf.placeholder("float", [None, action_num])
+		self.q_val = tf.placeholder("float", [None])
 		self.actions = tf.placeholder("float", [None, action_num])
-		q_vals = tf.reduce_sum(tf.multiply(self.y_hat, self.actions), 1)
-		self.loss = tf.reduce_sum(tf.square(self.labels - q_vals))
-		
+		q_val_hat = tf.reduce_sum(tf.multiply(self.y_hat, self.actions), 1)
+		loss = tf.reduce_sum(tf.square(self.q_val - q_val_hat))
+		self.train = tf.train.AdamOptimizer().minimize(loss)
+
+	def update(state, action, reward, next_state):
+		reward_vecs = self.sess.run(self.y_hat, {self.x: next_state})
+		q_vals = reward + gamma*np.amax(reward_vecs, 1)
+		self.sess.run(self.train, {self.x: state, self.q_val: q_vals, self.actions: action})
+
+	def action(state):
+		reward_vec = self.sess.run(self.y_hat, {self.x: state})
+		return np.argmax(reward_vec)
+
+
+
+
+
+
---
+++ imagination_architecture/model_based/ICNetTrain.py
@@ -0,0 +1,162 @@
+import tensorflow as tf
+from collections import deque
+import numpy as np
+import random
+import gym
+from math import log
+
+record = open("performance", "w")
+savefile = "./savefile.h5"
+
+# POLE-SPECIFIC
+max_time = 500
+
+gamma = 0.9
+class ICNet:
+    def __init__(self, sess, input_size, action_num):
+        self.sess = sess
+        self.x = tf.placeholder("float", [None, input_size])
+        W1 = tf.Variable(tf.random_uniform([input_size, 20], -0.5, 0.5))
+        b1 = tf.Variable(tf.random_uniform([20], -0.5, 0.5))
+        l1 = tf.nn.relu(tf.matmul(self.x, W1)+b1)
+        W2 = tf.Variable(tf.random_uniform([20, action_num], -0.5, 0.5))
+        b2 = tf.Variable(tf.random_uniform([action_num], -0.5, 0.5))
+        self.y_hat = tf.nn.relu(tf.matmul(l1, W2)+b2)
+        self.y = tf.placeholder("float", [None, action_num])
+        self.q_val = tf.placeholder("float", [None])
+        self.actions = tf.placeholder("float", [None, action_num])
+        q_val_hat = tf.reduce_sum(tf.multiply(self.y_hat, self.actions), 1)
+        loss = tf.reduce_sum(tf.square(self.q_val - q_val_hat))
+        self.train = tf.train.AdamOptimizer().minimize(loss)
+        self.saver = tf.train.Saver()
+
+    def update(self, state, action, reward, next_state):
+        reward_vecs = self.sess.run(self.y_hat, {self.x: next_state})
+        q_vals = reward + gamma*np.amax(reward_vecs, 1)
+        self.sess.run(self.train, {self.x: state, self.q_val: q_vals, self.actions: action})
+
+    def action(self, state):
+        reward_vec = self.sess.run(self.y_hat, {self.x: state})
+        return np.argmax(reward_vec)
+
+
+
+# Deep Q-learning Agent
+class DQNAgent:
+
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.model = self._build_model()
+        self.episodes = 500
+        self.training_result = []
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        session = tf.Session()
+        model = ICNet(session, self.state_size, self.action_size)
+        init = tf.global_variables_initializer()
+        model.sess.run(init)
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.action(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        states = []
+        actions = []
+        rewards = []
+        next_states = []
+        for tup in minibatch:
+            states.append(tup[0])
+            actions.append(tup[1])
+            rewards.append(tup[2])
+            next_states.append(tup[3])
+        states = np.array(states)
+        actions = np.eye(self.action_size)[actions]
+        rewards = np.array(rewards)
+        next_states = np.array(next_states)
+        self.model.update(states, actions, rewards, next_states)
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        self.model.saver.restore(self.model.sess, name)
+
+    def save(self, name):
+        self.model.saver.save(self.model.sess, name)
+
+    # Should be def train(self, agent_action)
+    def train(self):
+        env = gym.make('CartPole-v1')
+        print(env.observation_space, env.action_space)
+        # Iterate the game
+        for e in range(self.episodes):
+            # reset state in the beginning of each game
+            state = env.reset()
+            state = np.reshape(state, [1, 4])
+            # time_t represents each frame of the game
+            # Our goal is to keep the pole upright as long as possible until score of max_time
+            # the more time_t the more score
+            for time_t in range(max_time):
+                # turn this on if you want to render
+                env.render()
+                # Decide action
+                action = self.act(state)
+                # Advance the game to the next frame based on the action.
+                # Reward is 1 for every frame the pole survived
+                next_state, reward, done, _ = env.step(action)
+                
+                # POLE-SPECIFIC
+                if time_t == max_time - 1:
+                    reward = 150
+                elif done:
+                    reward = -5
+                else:
+                    reward = log(time_t + 1) / 10 + 1
+
+                next_state = np.reshape(next_state, [1, 4])
+                # Remember the previous state, action, reward, and done
+                self.remember(state, action, reward, next_state, done)
+                # make next_state the new current state for the next frame.
+                state = next_state
+                # done becomes True when the game ends
+                # ex) The agent drops the pole
+                if done:
+                    # print the score and break out of the loop
+                    print("episode: {}/{}, score: {}"
+                          .format(e, self.episodes, time_t))
+                    break
+            # train the agent with the experience of the episode
+            self.training_result.append(time_t)
+            num_mem = len(self.memory)
+            if num_mem > 32:
+                num_mem = 32
+            agent.replay(num_mem)
+        for e in self.training_result:
+            record.write(str(e) + " ")
+
+agent = DQNAgent(4,2)
+
+def train_agent():
+    agent.train()
+    agent.save(savefile)
+
+def load_agent():
+    agent.load(savefile)
+
+
+if __name__ == "__main__":
+    train_agent()
